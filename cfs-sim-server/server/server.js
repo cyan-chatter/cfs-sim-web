c42b8a57e34c3512423b82d6a38614d22af352fc
Sayan Chatterjee <sayanchatterjee2000@gmail.com>
2021-05-01 19:45:08 +0530

    core server algo

--- cfs-sim-server/server/server.js
+++ cfs-sim-server/server/server.js
@@ -2,53 +2,100 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const port = 5500
+const scheduler = require('../scheduler')
 
+var resultData = null
+
+const getResults = (timeTaken, results) => {
+    resultData = {
+        ...resultData, results, timeTaken
+    }
+}
 
 //write your code here
 const schedulerMain = (inputData) => {
-    
-    var answer = null
-    
-    //store the final answer in this response variable
-    //this will be JS object of the following format:
-    /* 
-    answer = {
-        finalAnswer : [ {RB Tree Instance 1}, {RB Tree Instance 2}, {RB Tree Instance 3} ] 
-    }
-    */
-    //you dont need to parse it to JSON String This server will automatically do that
-    //consider this function your main function
-
-
-
-
-    
-    return answer
+    var timeline = scheduler.getTimeline()
+    scheduler.runScheduler(inputData, timeline, getResults)
 }
 
-app.post('/', function (req, res) {
-  console.log(req.body)
-  const inputData = req.body
-  /* 
-    Input in this form:
+const inputData = {
+    num_of_tasks : 3,
+    total_time : 11,
+    task_queue : [
+        {id : 'A',
+        arrival_time : 1,
+        burst_time : 3,
+        priority : 2},
+        {id : 'B',
+        arrival_time : 2,
+        burst_time : 4,
+        priority : 1},
+        {id : 'C',
+        arrival_time : 2,
+        burst_time : 3,
+        priority : 3}
+    ]
+}
+
+console.log(inputData) 
+schedulerMain(inputData)
+
+const response = resultData
+
+console.log(response.results.time_data)
+console.log(response.results.node_stats)
+
+
+// app.post('/', function (req, res) {
+//   console.log(req.body)
+//   //const inputData = req.body
+//   /* 
+//     Input in this form:
     
-    {
-        number : "3",
-        tq : "11",
-        pd: [
-            {PID : "A", AT : "12", BT : "23"},
-            {PID : "A", AT : "12", BT : "23"},
-            {PID : "A", AT : "12", BT : "23"}
-        ]
-    }
+//     {
+//         number : "3",
+//         total_time : "11",
+//         pd: [
+//             {PID : "A", AT : "12", BT : "23"},
+//             {PID : "A", AT : "12", BT : "23"},
+//             {PID : "A", AT : "12", BT : "23"}
+//         ]
+//     }
 
-  */
+//     {num_of_tasks: num_of_tasks,
+//             total_time:   total_time,
+//             task_queue:   queue}
 
-    console.log(inputData) 
-    const response = schedulerMain(inputData)
-    console.log(response)
-    res.send(response)
-})
+// A 1 3
+// B 2 4
+// C 2 3
+
+//   */
+
+//     const inputData = {
+//         num_of_tasks : 3,
+//         total_time : 11,
+//         task_queue : [
+//             {id : 'A',
+//             arrival_time : 1,
+//             burst_time : 3,
+//             priority : 2},
+//             {id : 'B',
+//             arrival_time : 2,
+//             burst_time : 4,
+//             priority : 1},
+//             {id : 'C',
+//             arrival_time : 2,
+//             burst_time : 3,
+//             priority : 3}
+//         ]
+//     }
+
+//     console.log(inputData) 
+//     const response = schedulerMain(inputData)
+//     console.log(response)
+//     res.send(response)
+// })
 
 app.listen(port, () => {
     console.log("server is up at port " + port)
